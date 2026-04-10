113380c5a5191d2414d8f54db5ee16e6026b0576
Michael Delva <michael.delva@fishingcactus.com>
2026-04-10 09:48:17 +0200

    clang format

--- Source/SignificanceExtensions/Private/SEGetSignificanceComponent.cpp
+++ Source/SignificanceExtensions/Private/SEGetSignificanceComponent.cpp
@@ -5,215 +5,207 @@
 
 #include <SignificanceManager.h>
 
-USEGetSignificanceComponent::USEGetSignificanceComponent( const FObjectInitializer & object_initializer ) :
-    Super( object_initializer ),
-    bUseConcurrentPostUpdate( true ),
-    ComputationType( ESESignificanceComputationType::MaxDistance ),
-    FixedSignificance( 1.0f ),
-    MaxDistance( 3000.0f ),
-    bComponentImplementsPostSignificanceUpdate( false ),
-    bOwnerImplementsInterface( false ),
-    MaxDistanceSquared( 1.0f ),
-    LastComputedSignificance( 0.0f )
+USEGetSignificanceComponent::USEGetSignificanceComponent(const FObjectInitializer& object_initializer)
+    : Super(object_initializer)
+    , bUseConcurrentPostUpdate(true)
+    , ComputationType(ESESignificanceComputationType::MaxDistance)
+    , FixedSignificance(1.0f)
+    , MaxDistance(3000.0f)
+    , bComponentImplementsPostSignificanceUpdate(false)
+    , bOwnerImplementsInterface(false)
+    , MaxDistanceSquared(1.0f)
+    , LastComputedSignificance(0.0f)
 {
-    PrimaryComponentTick.bCanEverTick = false;
+	PrimaryComponentTick.bCanEverTick = false;
 
-    auto implemented_in_blueprint = [ & ]( const FName function_name ) -> bool {
-        const UFunction * function = GetClass()->FindFunctionByName( function_name );
+	auto implemented_in_blueprint = [&](const FName function_name) -> bool {
+		const UFunction* function = GetClass()->FindFunctionByName(function_name);
 
-        return function != nullptr && ensure( function->GetOuter() ) && function->GetOuter()->IsA( UBlueprintGeneratedClass::StaticClass() );
-    };
+		return function != nullptr && ensure(function->GetOuter()) && function->GetOuter()->IsA(UBlueprintGeneratedClass::StaticClass());
+	};
 
-    {
-        static auto func_name = FName( TEXT( "K2_GetSignificance" ) );
-        bComponentImplementsGetSignificance = implemented_in_blueprint( func_name );
-    }
-    {
-        static auto func_name = FName( TEXT( "K2_PostSignificanceUpdate" ) );
-        bComponentImplementsPostSignificanceUpdate = implemented_in_blueprint( func_name );
-    }
+	{
+		static auto func_name = FName(TEXT("K2_GetSignificance"));
+		bComponentImplementsGetSignificance = implemented_in_blueprint(func_name);
+	}
+	{
+		static auto func_name = FName(TEXT("K2_PostSignificanceUpdate"));
+		bComponentImplementsPostSignificanceUpdate = implemented_in_blueprint(func_name);
+	}
 }
 
-void USEGetSignificanceComponent::EndPlay( const EEndPlayReason::Type end_play_reason )
+void USEGetSignificanceComponent::EndPlay(const EEndPlayReason::Type end_play_reason)
 {
-    if ( auto * significance_manager = USignificanceManager::Get< USignificanceManager >( GetWorld() ) )
-    {
-        significance_manager->UnregisterObject( this );
-    }
+	if (auto* significance_manager = USignificanceManager::Get<USignificanceManager>(GetWorld()))
+	{
+		significance_manager->UnregisterObject(this);
+	}
 
-    Super::EndPlay( end_play_reason );
+	Super::EndPlay(end_play_reason);
 }
 
 #if WITH_EDITOR
-EDataValidationResult USEGetSignificanceComponent::IsDataValid( FDataValidationContext & context ) const
+EDataValidationResult USEGetSignificanceComponent::IsDataValid(FDataValidationContext& context) const
 {
-    switch ( ComputationType )
-    {
-        case ESESignificanceComputationType::Fixed:
-        {
-        }
-        break;
-        case ESESignificanceComputationType::MaxDistance:
-        {
-        }
-        break;
-        case ESESignificanceComputationType::DistanceThreshold:
-        {
-            if ( SignificanceDistances.IsEmpty() )
-            {
-                context.AddError( FText::FromString( TEXT( "SignificanceDistances is empty and you don't use a fixed significance" ) ) );
-            }
-            else
-            {
-                auto previous_distance = SignificanceDistances[ 0 ].DistanceThreshold;
-                for ( auto index = 1; index < SignificanceDistances.Num(); ++index )
-                {
-                    const auto current_distance = SignificanceDistances[ index ].DistanceThreshold;
-                    if ( current_distance <= previous_distance )
-                    {
-                        context.AddError( FText::FromString( TEXT( "Distances in SignificanceDistances must be sorted from closest to farthest" ) ) );
-                        break;
-                    }
+	switch (ComputationType)
+	{
+		case ESESignificanceComputationType::Fixed: {
+		}
+		break;
+		case ESESignificanceComputationType::MaxDistance: {
+		}
+		break;
+		case ESESignificanceComputationType::DistanceThreshold: {
+			if (SignificanceDistances.IsEmpty())
+			{
+				context.AddError(FText::FromString(TEXT("SignificanceDistances is empty and you don't use a fixed significance")));
+			}
+			else
+			{
+				auto previous_distance = SignificanceDistances[0].DistanceThreshold;
+				for (auto index = 1; index < SignificanceDistances.Num(); ++index)
+				{
+					const auto current_distance = SignificanceDistances[index].DistanceThreshold;
+					if (current_distance <= previous_distance)
+					{
+						context.AddError(FText::FromString(TEXT("Distances in SignificanceDistances must be sorted from closest to farthest")));
+						break;
+					}
 
-                    previous_distance = current_distance;
-                }
-            }
-        }
-        break;
-        default:
-        {
-            checkNoEntry();
-        };
-    }
+					previous_distance = current_distance;
+				}
+			}
+		}
+		break;
+		default: {
+			checkNoEntry();
+		};
+	}
 
-    return Super::IsDataValid( context );
+	return Super::IsDataValid(context);
 }
 #endif
 
 void USEGetSignificanceComponent::BeginPlay()
 {
-    Super::BeginPlay();
+	Super::BeginPlay();
 
-    MaxDistanceSquared = FMath::Square( MaxDistance );
+	MaxDistanceSquared = FMath::Square(MaxDistance);
 
-    bOwnerImplementsInterface = GetOwner()->GetClass()->ImplementsInterface( USEGetSignificanceInterface::StaticClass() );
+	bOwnerImplementsInterface = GetOwner()->GetClass()->ImplementsInterface(USEGetSignificanceInterface::StaticClass());
 
-    if ( auto * significance_manager = USignificanceManager::Get< USignificanceManager >( GetWorld() ) )
-    {
-        auto tag = SignificanceTag;
-        if ( tag == NAME_None )
-        {
-            tag = GetOwner()->GetClass()->GetFName();
-        }
+	if (auto* significance_manager = USignificanceManager::Get<USignificanceManager>(GetWorld()))
+	{
+		auto tag = SignificanceTag;
+		if (tag == NAME_None)
+		{
+			tag = GetOwner()->GetClass()->GetFName();
+		}
 
-        significance_manager->RegisterObject(
-            this,
-            tag,
-            [ & ]( const USignificanceManager::FManagedObjectInfo * managed_object_info, const FTransform & view_transform ) {
-                LastComputedSignificance = GetSignificance( managed_object_info, view_transform );
-                return LastComputedSignificance;
-            },
-            bUseConcurrentPostUpdate ? USignificanceManager::EPostSignificanceType::Concurrent : USignificanceManager::EPostSignificanceType::Sequential,
-            [ & ]( const USignificanceManager::FManagedObjectInfo * managed_object_info, float old_significance, float new_significance, bool is_final ) {
-                PostSignificanceUpdate( managed_object_info, old_significance, new_significance, is_final );
-            } );
-    }
+		significance_manager->RegisterObject(
+		    this,
+		    tag,
+		    [&](const USignificanceManager::FManagedObjectInfo* managed_object_info, const FTransform& view_transform) {
+			    LastComputedSignificance = GetSignificance(managed_object_info, view_transform);
+			    return LastComputedSignificance;
+		    },
+		    bUseConcurrentPostUpdate ? USignificanceManager::EPostSignificanceType::Concurrent : USignificanceManager::EPostSignificanceType::Sequential,
+		    [&](const USignificanceManager::FManagedObjectInfo* managed_object_info, float old_significance, float new_significance, bool is_final) {
+			    PostSignificanceUpdate(managed_object_info, old_significance, new_significance, is_final);
+		    });
+	}
 }
 
-float USEGetSignificanceComponent::K2_GetSignificance_Implementation( FName /*tag*/, const FTransform & /*transform*/ view_transform )
+float USEGetSignificanceComponent::K2_GetSignificance_Implementation(FName /*tag*/, const FTransform& /*transform*/ view_transform)
 {
-    return 1.0f;
+	return 1.0f;
 }
 
-void USEGetSignificanceComponent::K2_PostSignificanceUpdate_Implementation( FName /*tag*/, float /*old_significance*/, float /*new_significance*/, bool /*is_final*/ )
-{
-}
-
-void USEGetSignificanceComponent::PostSignificanceUpdate( FName tag, float old_significance, float new_significance, bool is_final )
+void USEGetSignificanceComponent::K2_PostSignificanceUpdate_Implementation(FName /*tag*/, float /*old_significance*/, float /*new_significance*/, bool /*is_final*/)
 {
 }
 
-float USEGetSignificanceComponent::GetSignificance( const USignificanceManager::FManagedObjectInfo * managed_object_info, const FTransform & view_transform )
+void USEGetSignificanceComponent::PostSignificanceUpdate(FName tag, float old_significance, float new_significance, bool is_final)
 {
-    if ( bOwnerImplementsInterface )
-    {
-        float significance;
-        if ( ISEGetSignificanceInterface::Execute_GetSignificance( GetOwner(), significance, managed_object_info->GetTag(), view_transform ) )
-        {
-            return significance;
-        }
-    }
-
-    if ( bComponentImplementsGetSignificance )
-    {
-        return K2_GetSignificance( managed_object_info->GetTag(), view_transform );
-    }
-
-    switch ( ComputationType )
-    {
-        case ESESignificanceComputationType::Fixed:
-        {
-            return FixedSignificance;
-        }
-        case ESESignificanceComputationType::MaxDistance:
-        {
-            return GetSignificanceByMaxDistance( view_transform );
-        }
-        case ESESignificanceComputationType::DistanceThreshold:
-        {
-            return GetSignificanceByDistanceThreshold( view_transform );
-        }
-        default:
-        {
-            checkNoEntry();
-            return 0.0f;
-        };
-    }
 }
 
-void USEGetSignificanceComponent::PostSignificanceUpdate( const USignificanceManager::FManagedObjectInfo * managed_object_info, float old_significance, float new_significance, bool is_final )
+float USEGetSignificanceComponent::GetSignificance(const USignificanceManager::FManagedObjectInfo* managed_object_info, const FTransform& view_transform)
 {
-    if ( bOwnerImplementsInterface )
-    {
-        if ( ISEGetSignificanceInterface::Execute_PostSignificanceUpdate( GetOwner(), managed_object_info->GetTag(), old_significance, new_significance, is_final ) )
-        {
-            return;
-        }
-    }
+	if (bOwnerImplementsInterface)
+	{
+		float significance;
+		if (ISEGetSignificanceInterface::Execute_GetSignificance(GetOwner(), significance, managed_object_info->GetTag(), view_transform))
+		{
+			return significance;
+		}
+	}
 
-    if ( bComponentImplementsPostSignificanceUpdate )
-    {
-        K2_PostSignificanceUpdate( managed_object_info->GetTag(), old_significance, new_significance, is_final );
-        return;
-    }
+	if (bComponentImplementsGetSignificance)
+	{
+		return K2_GetSignificance(managed_object_info->GetTag(), view_transform);
+	}
 
-    PostSignificanceUpdate( managed_object_info->GetTag(), old_significance, new_significance, is_final );
+	switch (ComputationType)
+	{
+		case ESESignificanceComputationType::Fixed: {
+			return FixedSignificance;
+		}
+		case ESESignificanceComputationType::MaxDistance: {
+			return GetSignificanceByMaxDistance(view_transform);
+		}
+		case ESESignificanceComputationType::DistanceThreshold: {
+			return GetSignificanceByDistanceThreshold(view_transform);
+		}
+		default: {
+			checkNoEntry();
+			return 0.0f;
+		};
+	}
 }
 
-float USEGetSignificanceComponent::GetSignificanceByDistanceThreshold( const FTransform & view_transform ) const
+void USEGetSignificanceComponent::PostSignificanceUpdate(const USignificanceManager::FManagedObjectInfo* managed_object_info, float old_significance, float new_significance, bool is_final)
 {
-    const auto actor_location = GetOwner()->GetActorLocation();
-    const auto distance = FVector::Dist( actor_location, view_transform.GetLocation() );
+	if (bOwnerImplementsInterface)
+	{
+		if (ISEGetSignificanceInterface::Execute_PostSignificanceUpdate(GetOwner(), managed_object_info->GetTag(), old_significance, new_significance, is_final))
+		{
+			return;
+		}
+	}
 
-    for ( auto index = SignificanceDistances.Num() - 1; index >= 0; index-- )
-    {
-        const auto significance_distance = SignificanceDistances[ index ];
-        if ( distance >= significance_distance.DistanceThreshold )
-        {
-            return significance_distance.Significance;
-        }
-    }
+	if (bComponentImplementsPostSignificanceUpdate)
+	{
+		K2_PostSignificanceUpdate(managed_object_info->GetTag(), old_significance, new_significance, is_final);
+		return;
+	}
 
-    return 1.0f;
+	PostSignificanceUpdate(managed_object_info->GetTag(), old_significance, new_significance, is_final);
 }
 
-float USEGetSignificanceComponent::GetSignificanceByMaxDistance( const FTransform & view_transform ) const
+float USEGetSignificanceComponent::GetSignificanceByDistanceThreshold(const FTransform& view_transform) const
 {
-    const auto actor_location = GetOwner()->GetActorLocation();
-    const auto distance_sqr = FVector::DistSquared( actor_location, view_transform.GetLocation() );
+	const auto actor_location = GetOwner()->GetActorLocation();
+	const auto distance = FVector::Dist(actor_location, view_transform.GetLocation());
 
-    auto significance = 1.0f - distance_sqr / MaxDistanceSquared;
-    significance = FMath::Clamp( 0.0f, significance, 1.0f );
+	for (auto index = SignificanceDistances.Num() - 1; index >= 0; index--)
+	{
+		const auto significance_distance = SignificanceDistances[index];
+		if (distance >= significance_distance.DistanceThreshold)
+		{
+			return significance_distance.Significance;
+		}
+	}
 
-    return significance;
+	return 1.0f;
+}
+
+float USEGetSignificanceComponent::GetSignificanceByMaxDistance(const FTransform& view_transform) const
+{
+	const auto actor_location = GetOwner()->GetActorLocation();
+	const auto distance_sqr = FVector::DistSquared(actor_location, view_transform.GetLocation());
+
+	auto significance = 1.0f - distance_sqr / MaxDistanceSquared;
+	significance = FMath::Clamp(0.0f, significance, 1.0f);
+
+	return significance;
 }
--- Source/SignificanceExtensions/Public/SEGetSignificanceComponent.h
+++ Source/SignificanceExtensions/Public/SEGetSignificanceComponent.h
@@ -12,93 +12,92 @@ struct FActorComponentTickFunction;
 UENUM()
 enum class ESESignificanceComputationType : uint8
 {
-    Fixed,
-    MaxDistance,
-    DistanceThreshold
+	Fixed,
+	MaxDistance,
+	DistanceThreshold
 };
 
-USTRUCT( BlueprintType )
+USTRUCT(BlueprintType)
 struct FSESignificanceDistance
 {
-    GENERATED_BODY()
+	GENERATED_BODY()
 
-    FSESignificanceDistance() :
-        Significance( 0.0f ),
-        DistanceThreshold( 0.0f )
-    {
-    }
+	FSESignificanceDistance()
+	    : Significance(0.0f), DistanceThreshold(0.0f)
+	{
+	}
 
-    UPROPERTY( EditAnywhere )
-    float Significance;
+	UPROPERTY(EditAnywhere)
+	float Significance;
 
-    UPROPERTY( EditAnywhere )
-    float DistanceThreshold;
+	UPROPERTY(EditAnywhere)
+	float DistanceThreshold;
 };
 
-UCLASS( Blueprintable, ClassGroup = ( Custom ), meta = ( BlueprintSpawnableComponent ) )
+UCLASS(Blueprintable, ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class SIGNIFICANCEEXTENSIONS_API USEGetSignificanceComponent : public UActorComponent
 {
-    GENERATED_BODY()
+	GENERATED_BODY()
 
 public:
-    explicit USEGetSignificanceComponent( const FObjectInitializer & object_initializer );
+	explicit USEGetSignificanceComponent(const FObjectInitializer& object_initializer);
 
-    float GetLastComputedSignificance() const
-    {
-        return LastComputedSignificance;
-    }
+	float GetLastComputedSignificance() const
+	{
+		return LastComputedSignificance;
+	}
 
-    void EndPlay( const EEndPlayReason::Type end_play_reason ) override;
+	void EndPlay(const EEndPlayReason::Type end_play_reason) override;
 
 #if WITH_EDITOR
-    EDataValidationResult IsDataValid( FDataValidationContext & context ) const override;
+	EDataValidationResult IsDataValid(FDataValidationContext& context) const override;
 #endif
 
-    void SetSignificanceTag( const FName tag );
+	void SetSignificanceTag(const FName tag);
 
 protected:
-    void BeginPlay() override;
+	void BeginPlay() override;
 
-    UFUNCTION( BlueprintNativeEvent, Category = "Significance" )
-    float K2_GetSignificance( FName tag, const FTransform & view_transform );
+	UFUNCTION(BlueprintNativeEvent, Category = "Significance")
+	float K2_GetSignificance(FName tag, const FTransform& view_transform);
 
-    UFUNCTION( BlueprintNativeEvent, Category = "Significance" )
-    void K2_PostSignificanceUpdate( FName tag, float old_significance, float new_significance, bool is_final );
+	UFUNCTION(BlueprintNativeEvent, Category = "Significance")
+	void K2_PostSignificanceUpdate(FName tag, float old_significance, float new_significance, bool is_final);
 
-    virtual void PostSignificanceUpdate( FName tag, float old_significance, float new_significance, bool is_final );
+	virtual void PostSignificanceUpdate(FName tag, float old_significance, float new_significance, bool is_final);
 
-    float GetSignificance( const USignificanceManager::FManagedObjectInfo * managed_object_info, const FTransform & view_transform );
-    void PostSignificanceUpdate( const USignificanceManager::FManagedObjectInfo * managed_object_info, float old_significance, float new_significance, bool is_final );
-    float GetSignificanceByDistanceThreshold( const FTransform & view_transform ) const;
-    float GetSignificanceByMaxDistance( const FTransform & view_transform ) const;
+	float GetSignificance(const USignificanceManager::FManagedObjectInfo* managed_object_info, const FTransform& view_transform);
+	void PostSignificanceUpdate(const USignificanceManager::FManagedObjectInfo* managed_object_info, float old_significance, float new_significance, bool is_final);
+	float GetSignificanceByDistanceThreshold(const FTransform& view_transform) const;
+	float GetSignificanceByMaxDistance(const FTransform& view_transform) const;
 
-    UPROPERTY( EditDefaultsOnly, Category = "Significance" )
-    uint8 bUseConcurrentPostUpdate : 1;
+	UPROPERTY(EditDefaultsOnly, Category = "Significance")
+	uint8 bUseConcurrentPostUpdate : 1;
 
-    UPROPERTY( EditDefaultsOnly, Category = "Significance" )
-    FName SignificanceTag;
+	UPROPERTY(EditDefaultsOnly, Category = "Significance")
+	FName SignificanceTag;
 
-    // Defines how to auto-calculate the significance. Note that if the actor or a child class implements ISEGetSignificanceInterface, then there is no auto-calculation
-    UPROPERTY( EditDefaultsOnly, Category = "Significance" )
-    ESESignificanceComputationType ComputationType;
+	// Defines how to auto-calculate the significance. Note that if the actor or a child class implements ISEGetSignificanceInterface, then there is no auto-calculation
+	UPROPERTY(EditDefaultsOnly, Category = "Significance")
+	ESESignificanceComputationType ComputationType;
 
-    UPROPERTY( EditDefaultsOnly, Category = "Significance", meta = ( EditCondition = "ComputationType == ESESignificanceComputationType::Fixed", ClampMin = "0.0", UIMin = "0.0", ClampMax = "1.0", UIMax = "1.0" ) )
-    float FixedSignificance;
+	UPROPERTY(EditDefaultsOnly, Category = "Significance", meta = (EditCondition = "ComputationType == ESESignificanceComputationType::Fixed", ClampMin = "0.0", UIMin = "0.0", ClampMax = "1.0", UIMax = "1.0"))
+	float FixedSignificance;
 
-    UPROPERTY( EditDefaultsOnly, Category = "Significance", meta = ( EditCondition = "ComputationType == ESESignificanceComputationType::DistanceThreshold" ) )
-    TArray< FSESignificanceDistance > SignificanceDistances;
+	UPROPERTY(EditDefaultsOnly, Category = "Significance", meta = (EditCondition = "ComputationType == ESESignificanceComputationType::DistanceThreshold"))
+	TArray<FSESignificanceDistance> SignificanceDistances;
 
-    UPROPERTY( EditDefaultsOnly, Category = "Significance", meta = ( EditCondition = "ComputationType == ESESignificanceComputationType::MaxDistance", ClampMin = "0.0", UIMin = "0.0", ForceUnits = "cm" ) )
-    float MaxDistance;
+	UPROPERTY(EditDefaultsOnly, Category = "Significance", meta = (EditCondition = "ComputationType == ESESignificanceComputationType::MaxDistance", ClampMin = "0.0", UIMin = "0.0", ForceUnits = "cm"))
+	float MaxDistance;
 
-    bool bComponentImplementsGetSignificance;
-    bool bComponentImplementsPostSignificanceUpdate;
-    bool bOwnerImplementsInterface;
-    float MaxDistanceSquared;
-    float LastComputedSignificance;
+	bool bComponentImplementsGetSignificance;
+	bool bComponentImplementsPostSignificanceUpdate;
+	bool bOwnerImplementsInterface;
+	float MaxDistanceSquared;
+	float LastComputedSignificance;
 };
 
-FORCEINLINE void USEGetSignificanceComponent::SetSignificanceTag( const FName tag )
+FORCEINLINE void USEGetSignificanceComponent::SetSignificanceTag(const FName tag)
 {
-    SignificanceTag = tag;
+	SignificanceTag = tag;
 }
